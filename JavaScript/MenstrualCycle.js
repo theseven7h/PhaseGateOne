97e752b472df343f22006b64e7c50b63448ff721
theseven7h <jamestauri123@gmail.com>
2025-06-06 22:33:14 +0100

    Menstrual cycle app implemented with functions to get next cycle date, ovulation date, fertile window, and safe period

--- JavaScript/MenstrualCycle.js
+++ JavaScript/MenstrualCycle.js
@@ -13,6 +13,29 @@ const getOvulationDate = function(date, cycleDays) {
 	return od;
 }
 
+const getFertileWindow = function(date, cycleDays) {
+	const fwb = getOvulationDate(date, cycleDays).minus({days: 5});
+	const fwa = getOvulationDate(date, cycleDays).plus({days: 1});
+	return [fwb,fwa];
+}
+
+const getSafePeriod = function(date, cycleDays) {
+	let lastCycle = DateTime.fromFormat(date, input);
+	let safePeriod = getFertileWindow(date, cycleDays);
+	let nextCycle = getNextCycleDate(date, cycleDays);
+	return [[lastCycle, safePeriod[0].minus({days: 1})],[safePeriod[1].plus({days: 1}), nextCycle]];
+}
+
+let lcd = DateTime.fromFormat("25/05/2025", input);
+let fertileWindow = getFertileWindow("25/05/2025", 28);
+let safePeriod = getSafePeriod("25/05/2025", 28);
+
+console.log("Last cycle date:", lcd.toFormat(output));
+
 console.log("Expected next cycle:", getNextCycleDate("25/05/2025", 28).toFormat(output));
 
 console.log("Expected ovulation:", getOvulationDate("25/05/2025", 28).toFormat(output));
+
+console.log("Expected fertile window:", fertileWindow[0].toFormat(output), "to", fertileWindow[1].toFormat(output));
+
+console.log("Expected safe period: " + safePeriod[0][0].toFormat(output), "to", safePeriod[0][1].toFormat(output), "and", safePeriod[1][0].toFormat(output), "to", safePeriod[1][1].toFormat(output));
