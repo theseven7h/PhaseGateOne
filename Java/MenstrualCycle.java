7f0172df78c832bfc95a2059939e854e9c5f024c
theseven7h <jamestauri123@gmail.com>
2025-06-06 16:19:09 +0100

    Menstrual cycle program updated with getNextCycleDateFunction with tests done

--- Java/MenstrualCycle.java
+++ Java/MenstrualCycle.java
@@ -1,3 +1,16 @@
-public class MenstrualCycle {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+public class MenstrualCycle {
+	static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	public static LocalDate getNextCycleDate(String input, int days) {
+		LocalDate od = LocalDate.parse(input, fmt).plusDays(days);
+		return od;
+	}
+	
+	public static void main(String[] args) {
+		DateTimeFormatter output = DateTimeFormatter.ofPattern("d MMMM, y");
+		System.out.print(getNextCycleDate("25/05/2025", 28).format(output));
+	}
 }
--- Java/TestMenstrualCycle.java
+++ Java/TestMenstrualCycle.java
@@ -1,6 +1,23 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class TestMenstrualCycle {
 
+	MenstrualCycle mc = new MenstrualCycle();	
+	static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+	@Test
+	public void testGetNextCycleDateFunctionExists() {
+		LocalDate next = mc.getNextCycleDate("25/05/2025", 28);
+		String actual = next.format(fmt);
+	}
+	
+	@Test
+	public void testGetNextCycleDateFunctionReturnsCorrectDay() {
+		LocalDate next = mc.getNextCycleDate("25/05/2025", 28);
+		String actual = next.format(fmt);
+		String expected = "22/06/2025";
+	}
 }
