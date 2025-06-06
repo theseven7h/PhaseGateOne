bfb9db6b47326ff9e90afd3abf4c2fca9ae0e922
theseven7h <jamestauri123@gmail.com>
2025-06-06 16:39:18 +0100

    Menstrual cycle program updated with getOvulationDate function tests started

--- Java/TestMenstrualCycle.java
+++ Java/TestMenstrualCycle.java
@@ -11,16 +11,27 @@ public class TestMenstrualCycle {
 	@Test
 	public void testGetNextCycleDateFunctionExists() {
 		LocalDate next = mc.getNextCycleDate("25/05/2025", 28);
-		String actual = next.format(fmt);
 	}
 	
 	@Test
-	public void testGetNextCycleDateFunctionReturnsCorrectDay() {
+	public void testGetNextCycleDateFunctionReturnsCorrectDate() {
 		LocalDate next = mc.getNextCycleDate("25/05/2025", 28);
 		String actual = next.format(fmt);
 		String expected = "22/06/2025";
 		assertEquals(actual, expected);
 	}
 	
+	@Test
+	public void testGetOvulationDateFunctionExists() {
+		LocalDate od = mc.getOvulationDate("25/05/2025", 28);
+	}
+	
+	@Test
+	public void testGetOvulationDateFunctionReturnsCorrectDate() {
+		LocalDate next = mc.getOvulationDate("25/05/2025", 28);
+		String actual = next.format(fmt);
+		String expected = "08/06/2025";
+		assertEquals(actual, expected);
+	}
 	
 }
